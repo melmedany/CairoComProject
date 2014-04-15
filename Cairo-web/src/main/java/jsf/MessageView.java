37bc37fdb414ce0ebea57cf35c63bc53af80c1a7
ferrycode <ferrycode@ferrycode-dev4>
2014-04-15 11:37:59 +0200

    first full version of jsf

---
+++ Cairo-web/src/main/java/jsf/MessageView.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jsf;
+
+import ejb.NewsEntity;
+import ejb.NewsEntityFacadeLocal;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author ferrycode
+ */
+@ManagedBean(name="MessageView")
+@RequestScoped
+
+public class MessageView {
+    @EJB
+    private NewsEntityFacadeLocal newsEntityFacade;
+    private NewsEntity NewsEntity;
+
+    /**
+     * Creates a new instance of MessageView
+     */
+    public MessageView() {
+         this.NewsEntity =new NewsEntity();
+    }
+    
+     public NewsEntity getMessage() {
+        return NewsEntity;
+    }
+     
+     public int getNumberOfMessages() {
+        return newsEntityFacade.findAll().size();
+    }
+     
+     public String postMessage() {
+        this.newsEntityFacade.create(NewsEntity);
+        return "theend";
+    }
+}
